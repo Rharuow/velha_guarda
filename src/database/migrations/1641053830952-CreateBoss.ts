f7a1d1437af2956b8806c1af1b1ef6ad913aaece
Harysson <haryssonsoares@gmail.com>
2022-01-01 13:26:40 -0300

    feat: migration to create boss

---
+++ src/database/migrations/1641053830952-CreateBoss.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateBoss1641053830952 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "boss",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "lvl_required",
+            type: "smallint",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("boss");
+  }
+}

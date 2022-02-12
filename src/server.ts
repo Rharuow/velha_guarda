84acc06c7b609e2219239ff641dff0b6006ea25b
Harysson <haryssonsoares@gmail.com>
2022-02-12 14:34:46 -0300

    fix: timezone to database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import pg from "pg";
 import app from "./app";
-
 import connection from "./database";
 
 app.listen(process.env.PORT || 3000, async () => {
+  pg.types.setTypeParser(
+    1114,
+    (stringValue) => new Date(`${stringValue}+0000`)
+  );
   await connection.create();
   console.log(`start server at ${process.env.HOST}:${process.env.PORT}`);
 });

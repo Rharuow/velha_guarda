36d1b8ce88612004e45f6f9327862b0f3d82c410
Harysson Soares <haryssonsoares@gmail.com>
2022-04-13 20:07:12 -0300

    feat: add sendgrid api to forgot password

--- src/services/User/ForgotPassword.ts
+++ src/services/User/ForgotPassword.ts
@@ -1,28 +1,34 @@
-import { getCustomRepository } from 'typeorm';
-import { hash } from 'bcryptjs'
+import { getCustomRepository } from "typeorm";
+import { hash } from "bcryptjs";
 
-import { UserRepository } from '../../repositories/UserRepository';
+import { UserRepository } from "../../repositories/UserRepository";
+import { sendForgotPassword } from "../../utils/sendgrid";
 
 export class ForgotPasswordUserService {
   async execute(email: string) {
     const userRepository = getCustomRepository(UserRepository);
 
     try {
-      const user = await userRepository.findOneOrFail({ email })
+      const user = await userRepository.findOneOrFail({ email });
 
-      const token = await hash(user.token, parseInt(process.env.HASH_SALTS))
+      const token = await hash(user.token, parseInt(process.env.HASH_SALTS));
 
-      await userRepository.update({ email }, { token })
+      await userRepository.update({ email }, { token });
+
+      await sendForgotPassword({
+        email: user.email,
+        name: user.name,
+        token,
+      });
 
       return {
         status: 200,
-        message: 'User ForgotPassword with success',
+        message: "User ForgotPassword with success",
         record: token,
       };
-
     } catch (error) {
       console.log(` = ${error.message}`);
       throw new Error(` = ${error.message}`);
-    };
+    }
   }
 }

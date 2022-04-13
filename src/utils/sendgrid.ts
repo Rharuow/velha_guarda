eb862af4b2e9b5b6892b6c790eebe698b3585be8
Harysson Soares <haryssonsoares@gmail.com>
2022-04-13 20:07:12 -0300

    feat: add sendgrid api to forgot password

--- src/utils/sendgrid.ts
+++ src/utils/sendgrid.ts
@@ -32,4 +32,30 @@ const sendConfirmationToken = async (data: {
   }
 };
 
-export { sendConfirmationToken };
+const sendForgotPassword = async (data: {
+  email: string;
+  name: string;
+  token: string;
+}) => {
+  const msg = {
+    to: data.email,
+    from: process.env.EMAIL_SENDGRID,
+    subject: "Confirmação de Cadastro",
+    text: `Alteração de senha`,
+    html: `<h1>Olá ${data.name}</h1>
+      <p>Clique no link abaixo para ALTERAR SUA SENHA:</p>
+      <p><strong style='color: red'>SE VOCÊ NÃO SOLICITOU A TROCA DE SENHA IGNORE ESSE EMAIL!</strong></p>
+      <a target="_blank" href="${process.env.WEB_URL}/change_password?email=${data.email}&token=${data.token}"> Trocar de senha </a>`,
+  };
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+  } catch (error) {
+    console.log("Email doesn't sent = ", error.message);
+    const userRepository = getCustomRepository(UserRepository);
+    await userRepository.delete({ email: data.email });
+    throw new Error(`Sendgrid: ${error.message}`);
+  }
+};
+
+export { sendConfirmationToken, sendForgotPassword };
